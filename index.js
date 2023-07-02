18b3e91ac08b1be24c4ac7685e5b143cd4ded0d4
Long Phan <longnphan@outlook.com>
2023-07-01 22:48:19 -0600

    add dealfirstHand, renderPlayerhand fn's

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
+const cardSuit = ["clubs", "diamonds", "hearts", "spades"];
+const cardValue = [
+  "ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "jack",
+  "queen",
+  "king",
+];
 const cardShoe = [];
+const playerHand = [];
 
 function createCardDeck() {
-  const cardSuit = ["clubs", "diamonds", "hearts", "spades"];
-  const cardValue = [
-    "ace",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "jack",
-    "queen",
-    "king",
-  ];
-
   for (let suit of cardSuit) {
     for (let val of cardValue) {
       cardShoe.push(`${suit}_${val}`);
     }
   }
   shuffleCards(cardShoe);
+}
+
+function dealFirstHand() {
+  for (let i = 0; i < 5; i++) {
+    let nextCard = cardShoe.shift();
+    console.log("next card removed is:", nextCard);
+    playerHand.push(nextCard);
+  }
+}
+
+function renderPlayerHand() {
+  for (let i = 0; i < playerHand.length; i++) {
+    let cardImage = document.createElement("img");
+    cardImage.setAttribute("src", `images/${playerHand[i]}.svg`);
+    dealer.appendChild(cardImage);
+  }
 }
 
 function shuffleCards(deck) {
@@ -35,4 +51,4 @@ function shuffleCards(deck) {
 }
 
 createCardDeck();
-console.log("card deck is", cardShoe.length);
+dealFirstHand();
